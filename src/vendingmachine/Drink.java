d94cd4d56d7c22630da2210a58ca4b74cb09a564
348848128 <348848128@109X0310BJXH6V1.EDU.YRDSB.NET>
2018-10-15 14:23:39 -0400

    Bill Cui

---
+++ src/vendingmachine/Drink.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
+
+/**
+ *
+ * @author 348848128
+ */
+public class Drink extends Snack implements Products{
+
+    int volume;
+    public Drink(String newName, String newDes, Double newPrice, int newAmt, int newVolume) {
+        super(newName, newDes, newPrice, newAmt);
+        volume = newVolume;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}

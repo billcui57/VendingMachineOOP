a10b55c14b2868a06b4e90653cd120b964a891cf
348848128 <348848128@109X0310BJXH6V1.EDU.YRDSB.NET>
2018-10-18 14:24:41 -0400

    Bill Cui

--- src/vendingmachine/Display.java
+++ src/vendingmachine/Display.java
@@ -9,44 +9,42 @@
 package vendingmachine;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 /**
  *
  * @author Adi Venkatesh
  */
-
-
 public class Display {
+
     DecimalFormat f = new DecimalFormat("#0.00");
     
     
-        public static void main(String[] args) {
-      
-            
-    }
-    
-    
-    public void listSnacks(VendingMachine machine){
+    public static void main(String[] args) {
+        VendingMachine machine = new VendingMachine(1000,7,7);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Vendobot");
+        listSnacks(machine);
+        System.out.println("Enter in snack index to purchase");
+        int index = input.nextInt()-1;
+        System.out.println("Selected "+ machine.makeSnackSelection(index).name + 
+                "\nPrice is $"+machine.makeSnackSelection(index).price+
+                "\nHow many would you like to buy?");
+        int amt = input.nextInt();
+        System.out.println("Your total is $"+(machine.makeSnackSelection(index).price * amt));
+        Change change =  machine.sellSnacks(machine.makeSnackSelection(index),input.nextDouble(), amt);
+        System.out.println("Your change is " + change.toString());
         
-        for(int i = 0; i<machine.numSnacks; i++){
-            System.out.println((i+1)+"\t"+machine.snacks[i].getName() +"\t" + machine.snacks[i].getDescription());
-            
+    }
+
+    public static void listSnacks(VendingMachine machine) {
+        System.out.println("Snack Index \t Snack Name \t Snack Description" );
+        for (int i = 0; i < machine.numSnacks; i++) {
+            System.out.println((i + 1) + "\t\t" + machine.snacks[i].getName() + "\t\t" + machine.snacks[i].getDescription());
         }
 
-}
-    
-    public void displaySnacks(){
-        
     }
+
     
-    
-    public void makeSelection(){
-        
-    }
-    
+
 }
-
-
-
-    
-    

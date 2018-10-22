ebb11e3583b42d2bed653fdfcedf879fbd2d6c69
billcui57 <billcui2001@hotmail.com>
2018-10-22 12:59:29 -0400

    Task: Turn all variables except for booleans to private and add getters and setters (ask me)

--- src/vendingmachine/Change.java
+++ src/vendingmachine/Change.java
@@ -35,28 +35,71 @@ public class Change {
     }
     
 
+    public int getToonies(){
+        return toonies;
+    }
+    
+    public int getLoonies(){
+        return loonies;
+    }
+    
+    public int getQuarters(){
+        return quarters;
+    }
+    
+    public int getDimes(){
+        return dimes;
+    }
+    
+    public int getNickels(){
+        return nickels;
+    }
+    
+    public double toDouble(){
+        return toonies*2+loonies*1+quarters*0.25+dimes*0.1+nickels*0.05;
+    }
     
     /**
      * 
      * @param amt amt of coin
      * @param type coin type
      */
-    public void add(String type,int amt){
+    public void add(String type,int amt) throws ChangeExceptions{
         switch(type){
             case "Toonie":
+                if(toonies+amt>=0){
                 toonies+=amt;
+                }else{
+                     throw new ChangeExceptions("Not enough toonies!");
+                }
                 break;
             case "Loonie":
+                if(loonies+amt>=0){
                 loonies+=amt;
+                }else{
+                     throw new ChangeExceptions("Not enough loonies!");
+                }
                 break;
             case "Quarter":
+                if(quarters+amt>=0){
                 quarters+=amt;
+                }else{
+                     throw new ChangeExceptions("Not enough quarters!");
+                }
                 break;
             case "Dime":
+                if(dimes+amt>=0){
                 dimes+=amt;
+                }else{
+                     throw new ChangeExceptions("Not enough dimes!");
+                }
                 break;
             case "Nickel":
+                if(nickels+amt>=0){
                 nickels+=amt;
+                }else{
+                     throw new ChangeExceptions("Not enough nickels!");
+                }
                 break;
         }
     }
@@ -65,7 +108,7 @@ public class Change {
      * 
      * @param type coin type
      */
-    public void add(String type){
+    public void add(String type) throws ChangeExceptions{
         add(type,1);
     }
     
@@ -91,7 +134,7 @@ public class Change {
      * @param type coin type
      * @param amt amt of coin
      */
-    public void remove(String type, int amt){
+    public void remove(String type, int amt) throws ChangeExceptions{
         add(type,amt*-1);
     }
     
@@ -99,15 +142,34 @@ public class Change {
      * 
      * @param amt amt of  coin
      */
-    public void remove(double amt){
+    public void remove(double amt) throws ChangeExceptions{
         Change temp = new Change(0);
         temp = denominate(amt);
-        toonies-=temp.toonies;
-        loonies-=temp.loonies;
-        quarters-=temp.quarters;
-        dimes-=temp.dimes;
-        nickels-=temp.nickels;
+        this.remove(temp);
     }
+    
+    public void remove(Change amt) throws ChangeExceptions{
+        
+        if(toonies-amt.toonies<0){
+            throw new ChangeExceptions("Not enough toonies!");
+        }else if(loonies-amt.loonies<0){
+            throw new ChangeExceptions("Not enough loonies!");
+        }else if(quarters-amt.quarters<0){
+            throw new ChangeExceptions("Not enough quarters!");
+        }else if(dimes-amt.dimes<0){
+            throw new ChangeExceptions("Not enough dimes!");
+        }else if(nickels-amt.nickels<0){
+            throw new ChangeExceptions("Not enough nickels!");
+        }else{
+            toonies-=amt.toonies;
+        loonies-=amt.loonies;
+        quarters-=amt.quarters;
+        dimes-=amt.dimes;
+        nickels-=amt.nickels;
+        }
+        
+    }
+    
     
     public String toString(){
         String change = "Toonies: " + toonies +"\tLoonies: " + loonies + 
@@ -150,6 +212,7 @@ public class Change {
      * @return temp
      */
     public Change findChange(double totalAmt,double givenAmt) throws ChangeExceptions{
+        
         if(givenAmt<totalAmt){
             throw new ChangeExceptions("Did not pay enough!");
         }else{
@@ -158,6 +221,7 @@ public class Change {
         return temp;
         }
     }
+    
     
     
     
